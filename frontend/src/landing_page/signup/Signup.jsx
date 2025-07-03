bfd6ee755b7adfa661eb467a2a77498d10e28a1a
Neeraj <neerajgoud1919@gmail.com>
2025-07-03 14:16:55 +0530

    resolved reidrects

--- frontend/src/landing_page/signup/Signup.jsx
+++ frontend/src/landing_page/signup/Signup.jsx
@@ -69,7 +69,7 @@ function Signup() {
 
       const token = res.data.message;
       localStorage.setItem("token", token);
-      window.location.href = `https://trade-hive-dashboard.vercel.app/dashboard?token=${token}`;
+      window.location.href = `https://trade-hive-dashboard.vercel.app?token=${token}`;
     } catch (error) {
       setErr(error.message);
     }
@@ -97,7 +97,7 @@ function Signup() {
 
       const token = res.data.message;
       localStorage.setItem("token", token);
-      window.location.href = `https://trade-hive-dashboard.vercel.app/dashboard?token=${token}`;
+      window.location.href = `https://trade-hive-dashboard.vercel.app?token=${token}`;
     } catch (error) {
       setErr(error.message);
     }
